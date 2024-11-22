c0c47ee4893a76ff845dd3f930591c87b9365798
pabrcno <pabrcn@gmail.com>
2024-11-22 20:07:07 -0300

    new deploy #3

--- .firebase/indigo-challenge-21-11-24/hosting/flutter_bootstrap.js
+++ .firebase/indigo-challenge-21-11-24/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2194438443"
+    serviceWorkerVersion: "3547527006"
   }
 });
